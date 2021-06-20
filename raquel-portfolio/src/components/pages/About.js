f511ed6a873b9cb42259e10436d5cad742fa79d2
rsco <RaquelLee@users.noreply.github.com>
2021-06-20 15:39:26 -0400

    portfolio updated

--- raquel-portfolio/src/components/pages/About.js
+++ raquel-portfolio/src/components/pages/About.js
@@ -14,9 +14,9 @@ const About = () => (
                     </img>
                 </div>
                 <div className="col s9">
-                    <h5>My name is Raquel Scofield, and I am a full-stack web developer and aspiring software engineer.</h5>
+                    <h5>My name is Raquel Scofield, and I am a full-stack web developer.</h5>
                     <p className="grey-text text-darken-3 lighten-3 ">
-                        I am currently 3/4 of the way through UCF's full stack web development program where I have found I have a passion for using my skills to develop applications that solve real-world problems, which you can view in my "Portfolio" section.</p>
+                        I recently earned a certificate in Full Stack Web Development from the Univercity of Central Florida where I have found I have a passion for using my skills to develop applications that solve real-world problems, which you can view in my "Portfolio" section.</p>
                     <div className="row">
                         <div className="section">
                             <div className="col m3">
