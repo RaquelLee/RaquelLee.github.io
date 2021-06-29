4774eab44afb7baeab75150d730a249fb8820d80
rsco <RaquelLee@users.noreply.github.com>
2021-06-29 11:47:06 -0400

    project updates

--- raquel-portfolio/src/components/Project.js
+++ raquel-portfolio/src/components/Project.js
@@ -1,27 +1,86 @@
-import React from "react";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import LinkIcon from '@material-ui/icons/Link';
+import GitHubIcon from '@material-ui/icons/GitHub';
+const useStyles = makeStyles((theme) => ({
+    media: {
+        height: 0,
+        paddingTop: '56.25%', // 16:9
+    },
+    expand: {
+        transform: 'rotate(0deg)',
+        marginLeft: 'auto',
+        transition: theme.transitions.create('transform', {
+            duration: theme.transitions.duration.shortest,
+        }),
+    },
+    expandOpen: {
+        transform: 'rotate(180deg)',
+    }
+}));
 
-function ProjectCard(props) {
+export default function ProjectCard(props) {
+    const classes = useStyles();
+    const [expanded, setExpanded] = React.useState(false);
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
+
     return (
+        <div className="section">
+
         <div className="col m6">
-            <div className="card z-depth-3 small">
-                <div className="card-image responsive-img">
+            <Card className={classes.root}>
+                <CardMedia
+                    className={classes.media}
+                    image={props.image}
+                    title={props.title}
+                />
+                <CardActions disableSpacing>
+                    <IconButton aria-label="add to favorites">
+                        
+                            <LinkIcon fontSize="large">
+                            <a href={props.github}></a>
+                            </LinkIcon>
                     
-                    <img alt={props.title} src={props.image}></img>
-                </div>
-                <div className="card-content">
-                    <span className="card-title center"><b>{props.title}</b></span>
-                    <div className="card-action center">
-                        <a href={props.github}>
-                            <i className="center fab fa-github-square"></i>
-                        </a>
-                        <a href={props.deployed}>
-                            <i className="valign-center fas fa-external-link-alt"></i>
-                        </a>
-                    </div>
-                </div>
-            </div>
+                    </IconButton>
+                    <IconButton aria-label="share">
+                    <GitHubIcon fontSize="large">
+                            <a href={props.github}></a>
+                            </GitHubIcon>
+                    </IconButton>
+                    <IconButton
+                        className={clsx(classes.expand, {
+                            [classes.expandOpen]: expanded,
+                        })}
+                        onClick={handleExpandClick}
+                        aria-expanded={expanded}
+                        aria-label="show more"
+                        
+                    >
+                        <ExpandMoreIcon fontSize="large"/>
+                    </IconButton>
+                </CardActions>
+                <Collapse in={expanded} timeout="auto" unmountOnExit>
+                    <CardContent>
+                        <Typography paragraph>Method:</Typography>
+                        <Typography paragraph>
+                            {props.description}
+                        </Typography>
+                    </CardContent>
+                </Collapse>
+            </Card>
+        </div>
         </div>
     );
 }
-
-export default ProjectCard;
