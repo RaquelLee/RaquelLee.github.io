fee9d948c3b20332a8e5fd7a9a452586f6ac0296
rsco <RaquelLee@users.noreply.github.com>
2021-07-28 11:40:42 -0400

    contact form code format

--- raquel-portfolio/src/components/ContactForm.js
+++ raquel-portfolio/src/components/ContactForm.js
@@ -4,31 +4,31 @@ const ContactForm = () => {
 	return (
 		<div className="card ">
 			<div className="card-content">
-		<form className=""
-			action="https://formspree.io/f/xeqvlwwa"
-			method="POST">
-			<div className="row">
-				<div classname="input-field">
-					<label for="email">Email</label>
-					<input type="email" name="_replyto" classname="validate" />
-				</div>
+				<form className=""
+					action="https://formspree.io/f/xeqvlwwa"
+					method="POST">
+					<div className="row">
+						<div classname="input-field">
+							<label for="email">Email</label>
+							<input type="email" name="_replyto" classname="validate" />
+						</div>
+					</div>
+					<div className="row">
+						<div classname="input-field col s12">
+							<label>
+								Message
+							</label>
+							<textarea className="materialize-textarea" name="message"></textarea>
+						</div>
+					</div>
+					<div className="row">
+						<button className="grey darken-3 right btn waves-effect waves-light" type="submit" name="action">
+							<i class="fa fa-paper-plane-o" aria-hidden="true"></i>
+						</button>
+					</div>
+				</form>
 			</div>
-			<div className="row">
-				<div classname="input-field col s12">
-					<label>
-						Message
-					</label>
-					<textarea className="materialize-textarea" name="message"></textarea>
-				</div>
-			</div>
-			<div className="row">
-				<button className="grey darken-3 right btn waves-effect waves-light" type="submit" name="action">
-				<i class="fa fa-paper-plane-o" aria-hidden="true"></i>
-				</button>
-			</div>
-		</form>
 		</div>
-	</div>
 	);
 };
 
