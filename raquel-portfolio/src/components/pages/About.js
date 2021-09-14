8c9459e0ce48083d001ccbd652c4ee647293a1b6
rsco <RaquelLee@users.noreply.github.com>
2021-09-14 14:17:16 -0400

    about me info added

--- raquel-portfolio/src/components/pages/About.js
+++ raquel-portfolio/src/components/pages/About.js
@@ -9,13 +9,19 @@ const About = () => (
             <div className="row container">
                 <div className="col s3">
                     <h1> </h1>
-                    <section><img alt="" src={me} className="responsive-img circle left z-depth-5">
-                    </img></section>
+                   <img alt="" src={me} className="responsive-img circle z-depth-5">
+                    </img>
                 </div>
                 <div className="col s9">
                     <h3>My name is Raquel Scofield, and I am a Full-Stack Web Developer.</h3>
                     <p className="grey-text text-darken-3 lighten-3 ">
-                        I recently earned a certificate in Full Stack Web Development from the University of Central Florida where I found I have a passion for using my skills to develop real-world applications, which you can view in my "Portfolio" section.
+                        I recently earned a certificate in Full Stack Web Development from the University of Central Florida where I found I have a passion for developing problem-solving &amp; real-world applications.
+                        
+                       
+                        Growing up on the Space Coast, I first discovered my interest in code at KSC Camp where I was most intrigued by the aspect of flight that took place “underneath the hood”, which inspired the galaxy theme of my portfolio. 
+                        
+                       
+My background as Ways and Means Board Chair and Auction Event Lead for a Bay Area co-op gave me a love for tight-knit and fast-paced working environments. I’m excited to join a team where I can contribute meaningfully and continue my path of learning to become a Software Engineer.
                     </p>
                     <div className="row">
                         <div className="section">
